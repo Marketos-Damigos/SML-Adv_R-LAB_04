0dcaf6f38dcb3119dd4a0bb28a28b5688fcacba6
Marketos-Damigos <anubis.coder@gmail.com>
2021-09-17 19:09:02 +0000

    Ext_print documentation

--- Lab4package/R/ext_print.R
+++ Lab4package/R/ext_print.R
@@ -1,3 +1,13 @@
+#' Print function
+#'
+#' @description Help function to print contents out of the linreg class
+#'
+#'
+#' @return Prints the content that is assigned
+#' @export ext_print
+
+
+
 ext_print <- function(...) {
     print(...)
 }
